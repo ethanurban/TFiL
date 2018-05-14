e4bc4e3bf68562ff08862f572f90b55b9c5663b2
Ethan Urban <buzzethanator@gmail.com>
2018-05-13 21:22:13 -0700

    Full main menu
    Gonna add a loading screen

--- Assets/Scripts/phaseOne.cs
+++ Assets/Scripts/phaseOne.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class levelManagerScript : MonoBehaviour {
 
     public bool settledP1, settledP2, settledP3, settledP4; //declaring bool that tells when each player is on a safe platform
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public GameObject p4;
 
 
 	// Use this for initialization
@@ -14,6 +18,11 @@ public class levelManagerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (settledP1)
+        {
+
+        }
         if (settledP1 && settledP2 && settledP3 && settledP4)//if all players are on a safe platform
         {
             //scene change time
